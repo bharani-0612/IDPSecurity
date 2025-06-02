f27851ce12fbc711f93b9286b70ccafca18a0c31
bharani-0612 <Bharanidharan.K3@cognizant.com>
2025-06-02 10:41:27 +0530

    Add files via upload

---
+++ backend/api-gateway/api-gateway/src/main/java/com/cognizant/config/GatewayCorsConfig.java
@@ -0,0 +1,26 @@
+package com.cognizant.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class GatewayCorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000"); // React frontend
+        config.addAllowedOrigin("http://localhost:3001");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*"); // Allow all headers
+        config.setAllowCredentials(true); // Allow cookies and credentials
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // Apply to all routes
+
+        return new CorsWebFilter(source);
+    }
+}
